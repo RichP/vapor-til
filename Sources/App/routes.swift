c1e049a05309587a5578099c838e772801ea8f05
Richard <r_b_pickup03@hotmail.com>
2018-10-20 07:36:01 +0100

    Bootstrap

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,15 +3,15 @@ import Fluent
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
-    
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
+//    // Basic "It works" example
+//    router.get { req in
+//        return "It works!"
+//    }
+//    
+//    // Basic "Hello, world!" example
+//    router.get("hello") { req in
+//        return "Hello, world!"
+//    }
     
     // Example of configuring a controller
     let acronymController = AcronymsController()
